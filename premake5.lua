adf9a060553d6b677b3f1cf47c2cf09abea4c761
Vikkes <viktor_schiff@web.de>
2023-11-27 06:00:16 +0100

    Implot added

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "ImPlot"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"implot.h",
+		"implot.cpp",
+		"implot_items.cpp",
+		"imgui_internal.h",
+	}
+
+	links
+	{
+		ImGui
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
